5a5eab6260705664c96c5f8c7880ab369a0ddb7a
tiagoLopes <saxtiago14@gmail.com>
2022-03-15 10:06:27 -0300

    Arquivos adicionados.

---
+++ src/main/java/com/palestraSpringSecurity/PalestraSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.palestraSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalestraSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalestraSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palestraSpringSecurity/PalestraSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palestraSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalestraSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
