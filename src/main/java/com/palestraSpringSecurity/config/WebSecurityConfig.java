0e5935f23b59a8b659ac8de6894e2ac5734a82c6
tiagoLopes <saxtiago14@gmail.com>
2022-03-15 12:04:21 -0300

    Login com página customizada.

--- src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest()
-                .authenticated().and().formLogin();
+                .authenticated().and().formLogin().loginPage("/entrar").permitAll();
     }
 
 }
--- src/main/java/com/palestraSpringSecurity/controller/GpController.java
+++ src/main/java/com/palestraSpringSecurity/controller/GpController.java
@@ -6,23 +6,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GpController {
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/")
+
+    @RequestMapping(method = RequestMethod.GET, path = "/entrar")
+    public String entrar(){
+        return "entrar";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, path = "/")
     public String inicio() {
         return "inicio";
     }
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/projetos")
+
+    @RequestMapping(method = RequestMethod.GET, path = "/projetos")
     public String projetos() {
         return "projetos";
     }
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/relatorio-custos")
+
+    @RequestMapping(method = RequestMethod.GET, path = "/relatorio-custos")
     public String relatorioCustos() {
         return "relatorio-custos";
     }
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/relatorio-equipe")
+
+    @RequestMapping(method = RequestMethod.GET, path = "/relatorio-equipe")
     public String relatorioEquipe() {
         return "relatorio-equipe";
     }
