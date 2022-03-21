bf6e766dff36d178b5075b92683b48f29f6a2a78
tiagoLopes <saxtiago14@gmail.com>
2022-03-21 00:24:00 -0300

    Adicionando @EnableJpaRepositories na classe void main da aplicação.

--- src/main/java/com/palestraSpringSecurity/PalestraSpringSecurityApplication.java
+++ src/main/java/com/palestraSpringSecurity/PalestraSpringSecurityApplication.java
@@ -1,9 +1,12 @@
 package com.palestraSpringSecurity;
 
+import com.palestraSpringSecurity.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class PalestraSpringSecurityApplication {
 
 	public static void main(String[] args) {
