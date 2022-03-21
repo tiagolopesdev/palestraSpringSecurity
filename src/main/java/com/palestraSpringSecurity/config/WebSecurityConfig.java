7a276a1a6358b3fcdb769ee005dbee5aa34eff18
tiagoLopes <saxtiago14@gmail.com>
2022-03-21 00:21:08 -0300

    Adicionando a configuração de AuthenticationManagerBuilder na classe WebSecurityConfig.

--- src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
@@ -4,17 +4,32 @@
  */
 package com.palestraSpringSecurity.config;
 
+import com.palestraSpringSecurity.security.UserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
  * @author tiagolopes
  */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailService userDetailService;
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +42,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin()
                 .loginPage("/entrar").permitAll()
                 .and().logout().logoutSuccessUrl("/entrar?logout").permitAll();
+    }
+    
+    @Bean 
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
