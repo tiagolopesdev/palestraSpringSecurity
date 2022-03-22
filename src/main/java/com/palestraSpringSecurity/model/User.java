162f41c6c323c5acf710105252d2115135daf06e
tiagoLopes <saxtiago14@gmail.com>
2022-03-22 19:53:08 -0300

    Adicionando FetchType.EAGER ao buscar as roles do user.

--- src/main/java/com/palestraSpringSecurity/model/User.java
+++ src/main/java/com/palestraSpringSecurity/model/User.java
@@ -26,7 +26,7 @@ public class User {
     private String password;
     private boolean active;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
             joinColumns
             = @JoinColumn(name = "user_id", referencedColumnName = "id"),
