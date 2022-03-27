f317ff885d998b6f0ccd85107535c51879067095
tiagoLopes <saxtiago14@gmail.com>
2022-03-27 15:03:14 -0300

    Adicionando o método AuthenticationManagerBuilder à classe WebSecurityConfig.

--- src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
@@ -4,11 +4,15 @@
  */
 package com.palestraSpringSecurity.config;
 
+import com.palestraSpringSecurity.repository.UserRepository;
 import com.palestraSpringSecurity.security.UserDetailService;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,14 +25,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailService userDetailService;
-    
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService);
+
+    @Autowired
+    protected void configurarGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
@@ -43,10 +48,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/entrar").permitAll()
                 .and().logout().logoutSuccessUrl("/entrar?logout").permitAll();
     }
-    
-    @Bean 
-    public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
+
 
 }
