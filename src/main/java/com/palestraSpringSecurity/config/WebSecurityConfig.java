42cc524b5e8718110cb109db682e876918c807fd
tiagoLopes <saxtiago14@gmail.com>
2022-03-29 11:10:06 -0300

    Configurações para a funcionalidade remember-me.

--- src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/palestraSpringSecurity/config/WebSecurityConfig.java
@@ -4,20 +4,16 @@
  */
 package com.palestraSpringSecurity.config;
 
-import com.palestraSpringSecurity.repository.UserRepository;
 import com.palestraSpringSecurity.security.UserDetailService;
-import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -45,8 +41,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and().formLogin()
-                .loginPage("/entrar").permitAll()
-                .and().logout().logoutSuccessUrl("/entrar?logout").permitAll();
+                    .loginPage("/entrar")
+                    .permitAll()
+                .and().logout()
+                    .logoutSuccessUrl("/entrar?logout")
+                    .permitAll()
+                .and().rememberMe()
+                    .userDetailsService(userDetailService);
     }
 
 
